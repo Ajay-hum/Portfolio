bca36df4b6a55913debcd941509564cb707f0d07
Ajay-hum <justuschimeremueze@gmail.com>
2025-01-21 01:36:38 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/new-repo-name/', // Update this to your repository name
   plugins: [react()],
 })
+
